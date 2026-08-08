207072a7dcfd306e202d375de384618418d1ab4b
Alex Denny <alexdenny25@gmail.com>
2026-08-08 19:33:27 -0400

    Continued work on security config

--- rainforest-api/src/main/java/rainforestapi/config/SecurityConfig.java
+++ rainforest-api/src/main/java/rainforestapi/config/SecurityConfig.java
@@ -1,13 +1,29 @@
+package rainforestapi.config;
 
-
-
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
 
-    
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception{
+        http
+            .csrf(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
+    }
 
 
 
