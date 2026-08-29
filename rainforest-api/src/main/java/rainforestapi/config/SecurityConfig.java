c9e66f79833fb0986674d9dbda5c713bb839eb86
Alex Denny <alexdenny25@gmail.com>
2026-08-29 16:35:37 -0400

    Continued work on implementing Security to the backend, need to add proper login so users can get authenticated

--- rainforest-api/src/main/java/rainforestapi/config/SecurityConfig.java
+++ rainforest-api/src/main/java/rainforestapi/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package rainforestapi.config;
 
+import java.util.List;
+
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,6 +14,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +28,7 @@ public class SecurityConfig{
         http.cors(Customizer.withDefaults());
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/Users").permitAll()
+                //.requestMatchers(HttpMethod.)
                 .requestMatchers("/error").permitAll()
                 .requestMatchers(HttpMethod.GET, "/Users/search").authenticated()
                 .requestMatchers(HttpMethod.GET, "/UsersAll").authenticated()
@@ -34,6 +41,20 @@ public class SecurityConfig{
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source; 
+    }
 
 
 
